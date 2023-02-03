b9b3d526cadabc0d8e04c550b974508c62c49063
stephen ligtenberg <bountyhunter24@gmail.com>
2023-02-03 17:57:38 -0500

    client side routinggit add .

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,5 @@
-import '../index.css';
 import React, { useState } from "react";
+import { Switch, Route } from "react-router-dom";
 import Header from "./Header";
 import NavBar from './NavBar';
 import ViewCars from './ViewCars';
@@ -9,19 +9,34 @@ function App() {
     const [viewMode, setViewMode] = useState("/view_cars")
 
     // switch between components
-    function viewModeControl() {
-      switch(viewMode) {
-        case "/view_cars": return <ViewCars />
-        case "/add_car": return <AddCar />
-        // case "/add_car": return <TBD />
-      }
-    }
+    // function viewModeControl() {
+    //   switch(viewMode) {
+    //     case "/view_cars": return <ViewCars />
+    //     case "/add_car": return <AddCar />
+    //     // case "/add_car": return <TBD />
+    //     default: return <h1>404 not found</h1>
+    //   }
+    // }
 
   return (
     <div>
       <Header />
       <NavBar setViewMode={setViewMode}/>
-      {viewModeControl()}
+      {/* {viewModeControl()} */}
+      <Switch>
+        <Route path="/view_cars">
+          <ViewCars />
+        </Route>
+        <Route path="/add_car">
+          <AddCar />
+        </Route>
+        <Route exact path="/">
+          <ViewCars />
+        </Route>
+        <Route path="*">
+          <h1>404 not found</h1>
+        </Route>
+      </Switch>
     </div>
   );
 }
