4ef63d060dabd71a811f0e86a87153a07a8b38fa
stephen ligtenberg <bountyhunter24@gmail.com>
2023-02-03 18:20:38 -0500

    programatic navigation

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function NavBar({ setViewMode }) {
     return (
-        <div id="nav-bar">
-            <button onClick={() => setViewMode("/view_cars")}>View Cars</button>
+        <nav id="nav-bar">
+            <Link to="/">View Cars</Link>
+            <Link to="/add_car">Add Car</Link>
+            <Link to="/TBD">TBD</Link>
+
+            {/* <button onClick={() => setViewMode("/view_cars")}>View Cars</button>
             <button onClick={() => setViewMode("/add_car")}>Add Car</button>
-            <button onClick={() => setViewMode("/TBD")}>TBD</button>
-        </div>
+            <button onClick={() => setViewMode("/TBD")}>TBD</button> */}
+        </nav>
     )
 }
 
